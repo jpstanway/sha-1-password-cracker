88960d6ec6eb1aa186b2503e67f163e274ce341c
Jordan S <jpstanway@gmail.com>
2022-05-05 23:11:33 -0700

    complete salt comparison

--- password_cracker.py
+++ password_cracker.py
@@ -1,43 +1,32 @@
 import hashlib
 
-def apply_salt(password, salt, append=False):
-  formatted_salt = salt.strip()
-  
-  if append:
-    with_salt = formatted_salt + password
-  else:
-    with_salt = password + formatted_salt
-  
-  encoded = str.encode(with_salt)
-  return encoded
+PW_FILE = 'top-10000-passwords.txt'
+SALT_FILE = 'known-salts.txt'
+DEFAULT_VALUE = 'PASSWORD NOT IN DATABASE'
 
-def get_hash(hash, encoded_str):
-  hash.update(encoded_str)
-  return hash.hexdigest()
 
 def crack_sha1_hash(hash, use_salts=False):
-  # get content of passwords text file
-  with open('top-10000-passwords.txt') as pws_file:
-    for password in pws_file:
-      pw_hash = hashlib.sha1()
-      formatted_password = password.strip()
-  
-      if use_salts:
-        # apply salts
-        with open('known-salts.txt') as salts_file:
-          for salt in salts_file:
-            appended_salt = apply_salt(formatted_password, salt, True)
-            prepended_salt = apply_salt(formatted_password, salt)
-            hex1 = get_hash(pw_hash, appended_salt)
-            hex2 = get_hash(pw_hash, prepended_salt)
-            
-            if hex1 == hash or hex2 == hash:
-              return formatted_password
-      else:
-        encoded = str.encode(formatted_password)
-        hex = get_hash(pw_hash, encoded)
-        
-        if hex == hash:
-          return formatted_password
+    # get content of passwords text file
+    with open(PW_FILE) as pws_file:
+        for password in pws_file:
+            f_password = password.strip()
+            pw_encoded = f_password.encode()
+            pw_hash = hashlib.sha1(pw_encoded).hexdigest()
 
-    return 'PASSWORD NOT IN DATABASE'
+            if use_salts:
+                # apply salts
+                with open(SALT_FILE) as salts_file:
+                    for salt in salts_file:
+                        f_salt = salt.strip()
+                        app_encoded = (f_password + f_salt).encode()
+                        pre_encoded = (f_salt + f_password).encode()
+                        app_hash = hashlib.sha1(app_encoded).hexdigest()
+                        pre_hash = hashlib.sha1(pre_encoded).hexdigest()
+
+                        if hash == app_hash or hash == pre_hash:
+                            return f_password
+            else:
+                if hash == pw_hash:
+                    return f_password
+
+        return DEFAULT_VALUE
